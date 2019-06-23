9c4ddcf75ca873adf712993ebf95d7a4bd2028e8
DilanLivera <DilanLivera@users.noreply.github.com>
2019-06-23 16:56:17 +1000

    get data from req.header

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,8 +8,9 @@ let ipaddress = "",
 router.route('/:whoami',)
   .get((req, res) => {
     software = req.header('User-Agent');
-    language = req.header('Accept-Language');
-    ipaddress = req.ip;
+    language = req.header('Accept-Language');    
+    ipaddress = req.header('x-forwarded-for').split(',')[0]; // can use req.connection.remoteAddress too
+    console.log(language);
 
     res.json({ ipaddress, language, software });
   });
